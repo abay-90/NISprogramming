e50d32b0296b2803861cba3a3f01e60883b89769
Abay0991 <76022004+Abay0991@users.noreply.github.com>
2021-01-20 21:09:57 +0100

    Deleted

--- Module 1/CalculateVolume_Answer.py
+++
@@ -1,6 +0,0 @@
-length = float(input("Please enter a length: "))
-width = float(input("Please enter a width: "))
-height = float(input("Please enter a height: "))
-
-volume = (length * width * height) * 101 / 100
-print("Volume: ", volume)
--- Module 1/TimeCalculation_Answer.py
+++
@@ -1,9 +0,0 @@
-minutes = int(input("Please enter the number of (whole) minutes worked this month: "))
-
-hours = int(minutes / 60)
-minutes = int(minutes % 60)
-days = int(hours / 8)
-hours = int(hours % 8)
-
-print("{0}:{1}:{2}".format(days, hours, minutes))
-
